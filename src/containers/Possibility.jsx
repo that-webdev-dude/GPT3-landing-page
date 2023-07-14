9e5f3c063bfa9d33c295845d2d264f9ad9654357
that.webdev.dude <that.webdev.dude@gmail.com>
2023-07-14 11:45:49 +0100

    possibility

--- src/containers/Possibility.jsx
+++ src/containers/Possibility.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./Possibility.css";
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div id="possibility" className="container">
+      <div className="possibility">
+        <div className="possibility-image">
+          <img src="" alt="possibility" />
+        </div>
+        <div className="possibility-content">
+          <a href="" className="possibility-content-link"></a>
+          <h2 className="possibility-content-heading"></h2>
+          <p className="possibility-content-text"></p>
+          <a href="" className="possibility-content-link"></a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
