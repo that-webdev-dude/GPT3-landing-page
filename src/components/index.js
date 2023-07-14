7a0920133ea9748cd680aa6b3b0fe5fabb196594
that.webdev.dude <that.webdev.dude@gmail.com>
2023-07-14 17:13:51 +0100

    logo restored

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Article } from "./Article";
 export { default as Brand } from "./Brand";
 export { default as CTA } from "./CTA";
 export { default as Feature } from "./Feature";
