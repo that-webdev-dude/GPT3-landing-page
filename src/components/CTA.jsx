e6b59c3bbdddcdab3d3ee5c4a5deed844553f151
that.webdev.dude <that.webdev.dude@gmail.com>
2023-07-14 15:39:50 +0100

    cta completed

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./CTA.css";
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div id="cta" className="container">
+      <div className="cta">
+        <div className="cta-heading">
+          <a href="">Request Early Access to Get Started</a>
+          <h2>Register today & start exploring the endless possiblities.</h2>
+        </div>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
