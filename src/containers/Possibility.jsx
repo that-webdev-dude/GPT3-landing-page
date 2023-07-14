4423d7050dc04002424facbfbeabb78e87715f63
that.webdev.dude <that.webdev.dude@gmail.com>
2023-07-14 13:02:29 +0100

    possibility completed

--- src/containers/Possibility.jsx
+++ src/containers/Possibility.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import "./Possibility.css";
+import possibilityImageURL from "../assets/possibility.png";
+
 const Possibility = () => {
   return (
     <div id="possibility" className="container">
       <div className="possibility">
         <div className="possibility-image">
-          <img src="" alt="possibility" />
+          <img src={possibilityImageURL} alt="possibility" />
         </div>
         <div className="possibility-content">
-          <a href="" className="possibility-content-link"></a>
-          <h2 className="possibility-content-heading"></h2>
-          <p className="possibility-content-text"></p>
-          <a href="" className="possibility-content-link"></a>
+          <div className="possibility-content-wrapper">
+            <a href="" className="possibility-content-link">
+              Request Early Access to Get Started
+            </a>
+            <h2 className="gradient__text ">
+              The possibilities are beyond your imagination
+            </h2>
+            <p className="possibility-content-text">
+              Yet bed any for travelling assistance indulgence unpleasing. Not
+              thoughts all exercise blessing. Indulgence way everything joy
+              alteration boisterous the attachment. Party we years to order
+              allow asked of.
+            </p>
+            <a href="" className="possibility-content-link">
+              Start Exploring
+            </a>
+          </div>
         </div>
       </div>
     </div>
